0a3a96705664d5e282ff20f869ffa65a85c8367b
Rudra Pratap Singh <merudra.official@gmail.com>
2024-01-14 20:09:08 +0530

    fc

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -18,10 +18,13 @@ function Root() {
   }
 
   // if (modal) {
-  //   disableScroll()
+  // //   disableScroll()
+  //     document.getElementsByTagName('body')[0].style.maxHeight="90%"
   // }
   // else {
-  //   enableScroll()
+  // //   enableScroll()
+  //   document.getElementsByTagName('body')[0].style.height="auto"
+
   // }
 
 
@@ -39,45 +42,6 @@ function Root() {
   )
 }
 
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; }
-  }));
-} catch (e) { }
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
-}
 
 
 export default Root
